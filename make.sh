48bc67a7719459a5271c4ee4ab6644d0e6c6c6a5
someanonimcoder <aiglubshev@edu.hse.ru>
2020-06-22 11:54:09 +0300

    make.sh and minor fixes

---
+++ make.sh
@@ -0,0 +1,7 @@
+pdflatex main.tex
+pdflatex main.tex
+
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=compressed_3.pdf main.pdf 
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=compressed_2.pdf main.pdf 
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=compressed_1.pdf main.pdf 
+
